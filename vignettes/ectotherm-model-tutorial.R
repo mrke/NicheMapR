19c0a70cca889682c7c5bb23637051a57764d0b3
mrke <mrke@unimelb.edu.au>
2015-11-14 11:55:04 +1100

    adding in extra environ variables
    Former-commit-id: 4b8e970dbf6d50980371ad361965519644565f54

--- vignettes/ectotherm-model-tutorial.R
+++ vignettes/ectotherm-model-tutorial.R
@@ -3,3 +3,17 @@ knitr::opts_chunk$set(
  eval = TRUE
 )
 
+## ------------------------------------------------------------------------
+library(NicheMapR)
+
+## ---- echo=FALSE---------------------------------------------------------
+source('../R/ectotherm.R')
+
+## ------------------------------------------------------------------------
+micro<-micro_global(loc="Kuranda, Queensland")
+ecto<-ectotherm()
+
+## ---- echo=FALSE, results='asis'-----------------------------------------
+knitr::kable(head(ecto$environ[,1:13], 12), digits = 2)
+knitr::kable(head(ecto$environ[,14:22], 12), digits = 2)
+
