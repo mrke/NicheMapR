8bad9189dcf38247fe3b19513552a5d62a7e8c1a
mrke <mrke@unimelb.edu.au>
2016-10-24 23:24:41 +1100

    adding TAIRhr to write_input output

--- R/micro_aust.R
+++ R/micro_aust.R
@@ -1752,6 +1752,7 @@ micro_aust <- function(loc="Nyrripi, Northern Territory",timeinterval=365,ystart
           write.table(L,file="micro csv input/L.csv", sep = ",", col.names = NA, qmethod = "double")
           write.table(LAI,file="micro csv input/LAI.csv", sep = ",", col.names = NA, qmethod = "double")
           write.table(tides,file="micro csv input/tides.csv", sep = ",", col.names = NA, qmethod = "double")
+          write.table(TAIRhr,file="micro csv input/RHhr.csv", sep = ",", col.names = NA, qmethod = "double")
           write.table(RHhr,file="micro csv input/RHhr.csv", sep = ",", col.names = NA, qmethod = "double")
           write.table(WNhr,file="micro csv input/WNhr.csv", sep = ",", col.names = NA, qmethod = "double")
           write.table(CLDhr,file="micro csv input/CLDhr.csv", sep = ",", col.names = NA, qmethod = "double")
