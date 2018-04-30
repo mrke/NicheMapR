f249ed3cb085d60d1ba441ad3c32e589d3c57570
mrke <mrke@unimelb.edu.au>
2018-04-30 17:26:57 +1000

    removing manual shade from micro_usa, and adding help for snow conductivity and interception parameters

--- R/micro_USA.R
+++ R/micro_USA.R
@@ -46,7 +46,6 @@
 #' \code{IUV}{ = 0, Use gamma function for scattered solar radiation? (computationally intensive)}\cr\cr
 #' \code{write_input}{ = 0, Write csv files of final input to folder 'csv input' in working directory? 1=yes, 0=no}\cr\cr
 #' \code{writecsv}{ = 0, Make Fortran code write output as csv files? 1=yes, 0=no}\cr\cr
-#' \code{manualshade}{ = 1, Use CSIRO Soil and Landscape Grid of Australia? 1=yes, 0=no}\cr\cr
 #' \code{terrain}{ = 0, Use 250m resolution terrain data? 1=yes, 0=no}\cr\cr
 #' \code{dailywind}{ = 1, Make Fortran code write output as csv files? 1=yes, 0=no}\cr\cr
 #' \code{windfac}{ = 1, factor to multiply wind speed by e.g. to simulate forest}\cr\cr
@@ -279,7 +278,7 @@ micro_usa <- function(loc = "Madison, Wisconsin", timeinterval = 365, ystart = 2
   aspect = 0, lapse_max = 0.0077, lapse_min = 0.0039,
   DEP=c(0, 2.5, 5, 10, 15, 20, 30, 50, 100, 200), minshade = 0, maxshade = 90,
   Refhyt = 2, Usrhyt = 0.01, Z01 = 0, Z02 = 0, ZH1 = 0, ZH2 = 0, runshade = 1,
-  clearsky = 0, rungads = 1, write_input = 0, writecsv = 0, manualshade = 1,
+  clearsky = 0, rungads = 1, write_input = 0, writecsv = 0,
   terrain = 0, dailywind = 1, windfac = 1, adiab_cor = 1, warm = 0,
   spatial = "N:/USA", ERR = 1.5, RUF = 0.004, EC = 0.0167238, SLE = 0.95,
   Thcond = 2.5, Density = 2.56, SpecHeat = 870, BulkDensity = 1.3, PCTWET = 0,
@@ -320,7 +319,6 @@ micro_usa <- function(loc = "Madison, Wisconsin", timeinterval = 365, ystart = 2
   # rungads=1
   # write_input=0
   # writecsv=0
-  # manualshade=1
   # terrain=0
   # dailywind=1
   # adiab_cor=1
@@ -1046,10 +1044,6 @@ micro_usa <- function(loc = "Madison, Wisconsin", timeinterval = 365, ystart = 2
     dim<-length(seq(ISOdate(ystart,1,1,tz=tzone)-3600*12, ISOdate((ystart+nyears),1,1,tz=tzone)-3600*13, by="days"))
     maxshades=rep(maxshade,dim)
     minshades=rep(minshade,dim)
-
-    if(manualshade==0){
-      maxshades <- static_soil_vars[,2:13] # assuming FAPAR represents shade
-    }
     shademax<-maxshades
 
     if(is.na(ALTITUDES)!=TRUE){
@@ -1104,16 +1098,9 @@ micro_usa <- function(loc = "Madison, Wisconsin", timeinterval = 365, ystart = 2
       WNMAXX <- Wind * windfac
       WNMINN <- Wind * windfac
 
-      if(manualshade==0){
-        maxshades1 <-spline(doys12,shademax,n=timeinterval,xmin=1,xmax=365,method="periodic")
-        MAXSHADES<-rep(maxshades1$y*100,nyears)
-        minshades <- rep(minshade,365)
-        minshades <- rep(minshades,nyears)
-        MINSHADES<-minshades
-      }else{
-        MAXSHADES<-maxshades
-        MINSHADES<-minshades
-      }
+      MAXSHADES<-maxshades
+      MINSHADES<-minshades
+
 
       REFLS <- rep(REFL, dim)
       PCTWET <- rep(PCTWET, dim)
