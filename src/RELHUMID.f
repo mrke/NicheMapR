2d2f71fa1e34bb1b9b66d5a93ab50a9452deea93
mrke <mrke@unimelb.edu.au>
2018-05-09 14:43:02 +1000

    including html vignettes
    Former-commit-id: 3501c61bcf5d2ca0584945900e7cce51b45b47a3

---
+++ src/RELHUMID.f
@@ -0,0 +1,62 @@
+      Subroutine RELHUMID
+
+c    This program uses 2 known temperatures and one known humidity to compute the other humidity.
+c    The temperatures are the reference air temperature, the local animal's air temperature and the reference humidity to compute the local humidity.
+
+      implicit none
+      double precision ALT,ALTT,BP,CP,DB,DENAIR,DP,E,ESAT,PATMOS,DENSTY
+      double precision RH,RHLOCL,THCOND,HTOVPR,TCOEFF,GGROUP,WB,DIFVPR
+      double precision SHAYD,SIOUT,MAXSHD,SABNEW,PTWET,VISKIN
+      double precision T,WC,C,DEP,OUT,RW,TVINC,TVIR,VAPREF,VD,VISDYN
+      double precision WTRPOT,rainfall
+
+      INTEGER I1,I2,I3,I4,I5,I6,I7,I8,I9,I10,I11,I12,ITEST,IOUT
+      integer I91,I92,I93,I94,I95,I96
+      INTEGER I97,I98,I99,I100,I101
+      
+      COMMON/GROUND/SHAYD,ALTT,MAXSHD,SABNEW,PTWET,rainfall
+      COMMON/SIUNIT/SIOUT(10)
+      COMMON/LOCLHUM/RHLOCL
+      COMMON/ARRAY/T(30),WC(20),C(20),DEP(30),IOUT(100),
+     1 OUT(101),ITEST(23)
+      COMMON/WMAIN/I1,I2,I3,I4,I5,I6,I7,I8,I9,I10,I11,I12,I91,I92,I93
+     & ,I94,I95,I96,I97,I98,I99,I100,I101
+
+c    setting variable values for sub's. dryair, wetair
+      WB = 0.
+      DP = 999.
+c    altitude, alt, known, but not barometric pressure, bp
+      ALT = ALTT
+      BP = 0.
+c    Dry bulb temperature is the 2m Tair
+      DB = OUT(2)
+
+c    call dryair to get atmospheric pressure, patmos, from altitude, alt, etc. for reference height
+      call DRYAIR(DB,BP,ALT,PATMOS,DENSTY,VISDYN,VISKIN,DIFVPR,
+     *THCOND,HTOVPR,TCOEFF,GGROUP)
+
+      RH = SIOUT(3)
+      if(RH.gt.100.)then
+            RH= 100.
+      endif
+c    get the vapor pressure, e, at Ta, 2m
+      call WETAIR(DB,WB,RH,DP,BP,E,ESAT,VD,RW,TVIR,TVINC,DENAIR,CP,
+     &  WTRPOT)
+      vapref = e
+c    get the saturation vapor pressure, esat, at Tlocal
+      RH = 100.
+      DB = SIOUT(2)
+      call WETAIR(DB,WB,RH,DP,BP,E,ESAT,VD,RW,TVIR,TVINC,DENAIR,CP,
+     *  WTRPOT)
+c    Definition of relative humidity using the vapor density at reference height
+      RHLOCL = (vapref/esat)* 100.
+
+      if(RHLOCL.gt.100.)then
+        RHLOCL = 100.
+      endif
+      if(RHLOCL.lt.0.000)then
+        RHLOCL = 0.01
+      endif
+
+      return
+      end
