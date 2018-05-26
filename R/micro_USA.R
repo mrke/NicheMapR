7eff2aa200e23a8ff0801898f39256ceadf95d42
mrke <mrke@unimelb.edu.au>
2018-05-26 17:15:29 +1000

    fixing typo in micro_usa
    Former-commit-id: d621336df98b96e7d32d5cf6d3c44a62330b73c6

--- R/micro_USA.R
+++ R/micro_USA.R
@@ -288,8 +288,7 @@ micro_usa <- function(loc = "Madison, Wisconsin", dstart = "01/01/2016", dfinish
   L = c(0, 0, 8.2, 8.0, 7.8, 7.4, 7.1, 6.4, 5.8, 4.8, 4.0, 1.8, 0.9, 0.6, 0.8, 0.4 ,0.4, 0, 0) * 10000,
   R1 = 0.001, RW = 2.5e+10, RL = 2e+6, PC = -1500, SP = 10, IM = 1e-06, MAXCOUNT = 500,
   LAI = 0.1, snowmodel = 1, snowtemp = 1.5, snowdens = 0.375, densfun = c(0.5979, 0.2178, 0.001, 0.0038),
-  snowmelt = 1, undercatch = 1, rainmelt = 0.0125, shore = 0,
-  tides = 0, as.POSIXct(dfinish, format = "%d/%m/%Y")), ncol = 3)),
+  snowmelt = 1, undercatch = 1, rainmelt = 0.0125, shore = 0, tides = 0,
   scenario = "", year = "", hourly = 0, rainhourly = 0, rainhour = 0,
   rainoff = 0, lamb = 0, IUV = 0, opendap = 1, soilgrids = 1, IR = 0, message = 0,
   fail = nyears * 24 * 365, save = 0, snowcond = 0, intercept = maxshade / 100 * 0.3) {
