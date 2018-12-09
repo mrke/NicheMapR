7f3d63044dfbd0aeff4354ce1460d465e3edd732
mrke <mrke@unimelb.edu.au>
2018-12-10 10:24:04 +1100

    micro_usa index change
    Former-commit-id: e5e87776c0dea00673ac17863049a6bef161cee6

--- R/micro_usa.R
+++ R/micro_usa.R
@@ -765,7 +765,7 @@ micro_usa <- function(
         index1 <- which.min(dist1)
         dist2 <- abs(lat - lat_1)
         index2 <- which.min(dist2)
-        start <- c(index2, index1, 1)
+        start <- c(index1, index2, 1)
         count <- c(1, 1, -1)
         for (j in 1:nyears) {
           if (j == 1) {
