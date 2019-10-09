c0e5e520f79a433c5a5e873986e8fc4c471bcd01
mrke <mrke@unimelb.edu.au>
2019-10-10 10:14:49 +1100

    new location for global climate.zip, cosmetic changes to get_p_wet

--- R/get_p_wet.R
+++ R/get_p_wet.R
@@ -15,14 +15,14 @@
 #' @param E_cut = 0.108 / 60 / 1000, Observed cutaneous evaporative water loss rate, g/s
 #' @param E_resp = 0.0371 / 60 / 1000, Observed respiratory evaporative water loss rate, g/s
 #' @param E_eye = 0.199 / 60 / 1000, Observed ocular evaporative water loss rate, g/s
-#' @param mass.g = 26.7, Mass of animal, g
+#' @param mass.g = 26.7, Wet mass of animal, g
 #' @param A_tot = 0.00894, Total surface area, m2
 #' @param fcond = 0.25, Fraction of total area area touching surfaces, dec\% (therefore no evaporation or convection)
 #' @param feye = 0.002, Fraction of total area area comprising the eye
 #' @param eye_frac = 0.58, Fraction of time eye is open, dec\%
-#' @param T_b = 25, Core body temperature, deg C
-#' @param T_s = 24.99, Skin (surface) temperature, deg C
-#' @param T_a = 24.98, Air temperature, deg C
+#' @param T_b = 25, Core body temperature, °C
+#' @param T_s = 24.99, Skin (surface) temperature, °C
+#' @param T_a = 24.98, Air temperature, °C
 #' @param vel = 0.0021, wind speed, m / s
 #' @param RHex = 100, relative humidity of exhaled air, \%
 #' @param RHin = 30, relative humidity of atmosphere, \%
@@ -32,9 +32,9 @@
 #' @param M_2 = 0.800, Metabolic rate equation parameter 2
 #' @param M_3 = 0.038, Metabolic rate equation parameter 3
 #' @param V_O2_STP = M_1 * mass.g ^ M_2 * 10 ^ (M_3 * T_b) / 3600 / 1000, Oxygen consumption rate corrected to STP, L O2 / s
-#' @param T_ref_O2 = 25, Reference temperature for oxygen consumption rate correction (deg C)
+#' @param T_ref_O2 = 25, Reference temperature for oxygen consumption rate correction (°C)
 #' @param V_air_STP = V_O2_STP / (0.2094 * E_O2 / 100), Ventilation rate at STP, L/s
-#' @param T_ref_vent = 25, Reference temperature for ventilation rate correction (deg C)
+#' @param T_ref_vent = 25, Reference temperature for ventilation rate correction (°C)
 #' @param rho_flesh = 1000, Density of flesh (kg/m3)
 #' @param T_A = 8817, Arhhenius temperature (K)
 #' @param T_AL = 50000, Arrhenius temperature for decrease below lower boundary of tolerance range T_L (K)
@@ -43,77 +43,58 @@
 #' @param T_H = 306, Upper boundary (K) of temperature tolerance range for Arrhenius thermal response
 #' @param alt = 0.03, Altitude (km)
 #' @param bp	= 101325, Barometric pressure (pascal)
-#' @return E_tot
-#' @return E_cut
-#' @return E_resp
-#' @return E_eye
-#' @return R_a
-#' @return R_s
-#' @return A_tot
-#' @return A_eye
-#' @return A_eff
-#' @return A_wet
-#' @return p_wet
+#' @return E_tot total evaporative water loss, g/s
+#' @return E_cut cutaneous evaporative water loss, g/s
+#' @return E_resp respiratory evaporative water loss, g/s
+#' @return E_eye ocular evaporative water loss, g/s
+#' @return R_a boundary layer resitance, s/m
+#' @return R_s skin resistance, s/m
+#' @return A_tot total surface area, m2
+#' @return A_eye ocular surface area, m2
+#' @return A_eff effective area for evaporation, m2
+#' @return A_wet area acting as a free water surface, m2
+#' @return p_wet proportion of surface area acting as a free water surface, m2
 #' @usage get_p_wet(cut.known = 1, E_tot = 0.344 / 60 / 1000, E_cut = 0.108 / 60 / 1000, T_b = 25, RHin = 30, vel = 0.0021)
-#' @examples
-#' library(NicheMapR)
-#'
-#'
 #' @export
-get_p_wet <- function(cut.known = 0, resp.known = 0, eye.known = 0, vent.known = 0,
-  press.known = 0, A_tot.known = 0,  E_tot = 0.344 / 60 / 1000,
-  E_cut = 0.108 / 60 / 1000, E_resp = 0.0371 / 60 / 1000,
-  E_eye = 0.199 / 60 / 1000, mass.g = 26.7, A_tot = 0.00894,
-  fcond = 0.25, feye = 0.002, eye_frac = 0.58, T_b = 25, T_s = 25 - 0.001,
-  T_a = 25 - 0.002, vel = 0.0021, RHex = 100, RHin = 30, E_O2 = 15,
-  RQ = 0.766,  M_1 = 0.013, M_2 = 0.8, M_3 = 0.0038,
-  V_O2_STP = M_1 * mass.g ^ M_2 * 10 ^ (M_3 * T_b) / 3600 / 1000,
-  T_ref_O2 = 30, V_air_STP = V_O2_STP / (0.2094 * E_O2 / 100), rho_flesh = 1000,
-  T_ref_vent = 30, T_A = 8817, T_AL = 50000, T_AH = 90000, T_L = 279, T_H = 306,
-  alt = 0, bp = 101325) {
-
-# cut.known = 0 # Is cutaneous water loss supplied? 0 = no (invokes estimation by partitioning respiratory), 1 = yes
-# resp.known = 0 # Is respiratory water loss supplied? 0 = no (invokes estimation by partitioning respiratory), 1 = yes
-# eye.known = 0 # Is ocular water loss supplied? 0 = no (invokes estimation by ocular surface area paritioning), 1 = yes
-# vent.known = 0 # Is ventilation rate supplied? 0 = no (rely on estimate from V_O2), 1 = yes
-# press.known = 0 # Is barometric pressure known (1, uses altitude) or has it been provided as the 'bp' variable (0)
-# A_tot.known = 0 # Is total surface area known? 0 = no (uses allometric equation for Dipsosaurus dorsalis, the desert iguana), 1 = yes
-# A_head.known = 0 # Is head surface area known? 0 = no (assumes x %), 1 = yes
-# E_tot = 0.344 / 60 / 1000 # Observed total evaporative water loss rate, g/s
-# E_cut = 0.108 / 60 / 1000 # Observed cutaneous evaporative water loss rate, g/s
-# E_resp = 0.0371 / 60 / 1000 # Observed respiratory evaporative water loss rate, g/s
-# E_eye = 0.199 / 60 / 1000 # Observed ocular evaporative water loss rate, g/s
-# mass.g = 26.72667 # Mass of animal, g
-# A_tot = 0 # Total surface area, m2
-# fcond = 0.25 # Fraction of total area area touching surfaces, dec% (therefore no evaporation or convection)
-# feye = 0.002054259 # Fraction of total area area comprising the eye
-# eye_frac = 0.58 # Fraction of time eye is open, dec%
-# T_b = 25 # Core body temperature, deg C
-# T_s = 24.99 # Skin (surface) temperature, deg C
-# T_a = 24.98 # Air temperature, deg C
-# vel = 0.0021 # wind speed, m / s
-# RHex = 100 # relative humidity of exhaled air, %
-# RHin = 30 # relative humidity of atmosphere, %
-# E_O2 = 15 # Oxygen extraction efficiency, %
-# RQ = 0.766 # Respiratory quotient, dec%
-# M_1 = 0.013 # Metabolic rate equation parameter 1 V_O2 = M_1 * M ^ M_2 * 10 ^ (M_3 * T_b) based on Eq. 2 from Andrews & Pough 1985. Physiol. Zool. 58:214-231
-# M_2 = 0.800 # Metabolic rate equation parameter 2
-# M_3 = 0.038 # Metabolic rate equation parameter 3
-# V_O2_STP = 0.0209  / 60 / 1000 # M_1 * mass.g ^ M_2 * 10 ^ (M_3 * T_b) / 3600 / 1000#Oxygen consumption rate corrected to STP, L O2 / s
-# T_ref_O2 = 25 # Reference temperature for oxygen consumption rate correction (deg C)
-# V_air_STP = V_O2_STP / (0.2094 * E_O2 / 100) # Ventilation rate at STP, L/s
-# T_ref_vent = 25 # Reference temperature for ventilation rate correction (deg C)
-# rho_flesh = 1000 # Density of flesh (kg/m3)
-# T_A = 8817 # Arhhenius temperature (K)
-# T_AL = 50000 # Arrhenius temperature for decrease below lower boundary of tolerance range T_L (K)
-# T_AH = 90000 # Arrhenius temperature for decrease above upper boundary of tolerance range T_H (K)
-# T_L = 279 # Lower boundary (K) of temperature tolerance range for Arrhenius thermal response
-# T_H = 306 # Upper boundary (K) of temperature tolerance range for Arrhenius thermal response
-# alt = 0.03 # Altitude (km)
-# bp	= 101325 # Barometric pressure (pascal)
-
+get_p_wet <- function(cut.known = 0,
+                      resp.known = 0,
+                      eye.known = 0,
+                      vent.known = 0,
+                      press.known = 0,
+                      A_tot.known = 0,
+                      E_tot = 0.344 / 60 / 1000,
+                      E_cut = 0.108 / 60 / 1000,
+                      E_resp = 0.0371 / 60 / 1000,
+                      E_eye = 0.199 / 60 / 1000,
+                      mass.g = 26.7,
+                      A_tot = 0.00894,
+                      fcond = 0.25,
+                      feye = 0.002,
+                      eye_frac = 0.58,
+                      T_b = 25,
+                      T_s = T_b - 0.001,
+                      T_a = T_b - 0.002,
+                      vel = 0.0021,
+                      RHex = 100,
+                      RHin = 30,
+                      E_O2 = 15,
+                      RQ = 0.766,
+                      M_1 = 0.013,
+                      M_2 = 0.8,
+                      M_3 = 0.0038,
+                      V_O2_STP = M_1 * mass.g ^ M_2 * 10 ^ (M_3 * T_b) / 3600 / 1000,
+                      T_ref_O2 = 30,
+                      V_air_STP = V_O2_STP / (0.2094 * E_O2 / 100),
+                      rho_flesh = 1000,
+                      T_ref_vent = 30,
+                      T_A = 8817,
+                      T_AL = 50000,
+                      T_AH = 90000,
+                      T_L = 279,
+                      T_H = 306,
+                      alt = 0,
+                      bp = 101325) {
   require(NicheMapR)
-
   # constants
   fO2_ref <- 0.2095	# ref O2 proportion, dec%
   fO2 <- 0.2095	# percent O2, dec%
@@ -182,19 +163,17 @@ get_p_wet <- function(cut.known = 0, resp.known = 0, eye.known = 0, vent.known =
     E_resp <- J_H20_resp * 18 # respiratory evaporative water, g/s (moles lost * gram molecular weight water)
   }
 
-  #
   if(cut.known == 0){
     E_cut <- E_tot - E_resp # cutaneous evaporative water loss, g/s
   }
 
   # compute convection and mass transfer coefficients using the Colburn analogy
-
   VOL <- mass.g / 1000 / rho_flesh # animal volume
   D <- VOL ^ (1/3) # m, characteristic dimension for convection
   if(A_tot.known == 0){
     # equation for Dipsosaurus dorsalis, from Porter et al. 1973, Oecologia
     #A_tot <- (10.4713 * mass ^ 0.688) / 10000 # total surface area, m2
-    A_tot <- 10 * mass.g ^ (2 / 3) / 100^2
+    A_tot <- 10 * mass.g ^ (2 / 3) / 100 ^ 2
   }
   A_eye <- A_tot * feye
   A_conv <- A_tot * (1 - fcond)	# area for convection, m2
@@ -202,23 +181,23 @@ get_p_wet <- function(cut.known = 0, resp.known = 0, eye.known = 0, vent.known =
   # free convection
   PR_free <- C_P * mu / k_air # Prandlt number for free convection, -
   SC_free <- mu / (rho_dry * D_H2O) # Schmidt number for free convection, -
-  delta_T <- T_s - T_a # temperature difference between skin and air, deg C
-  if(abs(delta_T) < 0.01){delta_T <- 0.01 * sign(delta_T)} # impose lower limit of 0.01 deg C
+  delta_T <- T_s - T_a # temperature difference between skin and air, °C
+  delta_T[abs(delta_T) < 0.01] <- 0.01 * sign(delta_T[abs(delta_T) < 0.01]) # impose lower limit of 0.01 °C
   GR <- abs(((rho_dry ^ 2) * BETA * G * (D ^ 3) * delta_T) / (mu ^ 2)) # Grashof number, -
   RE <- rho_dry * vel * D / mu # Reynold's number, -
   RA <- (GR ^ (1/4)) * (PR_free ^ (1/3)) # Rayleigh number, - (formula for sphere or lizard)
   NU_free <- 2 + 0.60 * RA # Nusslet number for free convection, -
   HC_free <- (NU_free * k_air) / D	# Heat transfer coeff for free convection, W/(m2 K)
-  if(HC_free < 5){HC_free <- 5} # don't let it go below 5, as this is unrealistic
+  HC_free[HC_free < 5] <- 5 # don't let it go below 5, as this is unrealistic
   NU_free <- HC_free * D / k_air # recalcualting NU_free in case HC_free had gone below lower limit of 5
   SH_free <- NU_free * (SC_free / PR_free) ^ (1/3) # Sherwood number, -
   HD_free <- SH_free * D_H2O / D # mass transfer coeff for free convection, m/s
   #Q_conv_free <- HC_free * convar * (T_s - T_a) # Free convective heat loss at skin, W
 
   # forced convection
-  PR_forc <-	0.72 # Prandlt number for forced convection, - (FROM ANCORR)
-  SC_forc <-	0.6 # Schmidt number for forced convection, - (FROM ANCORR)
-  NU_forc <- 0.35 * RE ^ 0.6	# Nusslet number for forced convection, - (FROM ANCORR)
+  PR_forc <-	0.72 # Prandlt number for forced convection, -
+  SC_forc <-	0.6 # Schmidt number for forced convection, -)
+  NU_forc <- 0.35 * RE ^ 0.6	# Nusslet number for forced convection, -
   HC_forc <- NU_forc * k_air / D # Heat transfer coeff for forced convection, W/(m2 K)
   SH_forc <- NU_forc* (SC_forc / PR_forc) ^ (1 / 3) # Sherwood number, -
   HD_forc <-	SH_forc * D_H2O / D	# mass transfer coeff, m/s
@@ -232,14 +211,14 @@ get_p_wet <- function(cut.known = 0, resp.known = 0, eye.known = 0, vent.known =
   # ocular water loss
   if(eye.known  == 0){
     E_eye <- (A_eye * eye_frac) * HD * (rho_H2O_skin - rho_H2O_air) / 1000 # g/s
-    E_cut <- E_cut - E_eye # take any ocular water loss from the estimate of cutaneous
+    E_cut <- E_cut - E_eye # subtract any ocular water loss from the estimate of cutaneous
   }
 
-  # compute actual skin resistance (unlike pct_wet, this does not include eyes)
+  # compute actual skin resistance (unlike p_wet, this does not include eyes)
   R_a <- rho_dry * C_P / HC # boundary layer resistance, s/m
   R_s <- (rho_H2O_skin - rho_H2O_air) / ((E_cut / 1000)/(A_tot - A_eye)) - R_a # skin resistance, s/m
 
-  # compute % of skin area that is acting like a free-water surface, p_wet, and associated areas
+  # compute proportion of skin area that is acting like a free-water surface, p_wet, and associated areas
   A_eff <- (E_cut / 1000) / (HD * (rho_H2O_skin - rho_H2O_air)) # effective area for evaporation, m2
   p_wet = A_eff / (A_tot - A_tot * fcond - A_eye * eye_frac + A_eye * (1 - eye_frac))
   A_wet = p_wet * A_tot
