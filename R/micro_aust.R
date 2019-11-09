688e8aed2201a4547382ab0c0a3f5d33d7887000
mrke <mrke@unimelb.edu.au>
2019-11-10 10:04:42 +1100

    updating warning message for opendap mode of micro_aust

--- R/micro_aust.R
+++ R/micro_aust.R
@@ -432,9 +432,8 @@ micro_aust <- function(
   errors<-0
 
   # error trapping - originally inside the Fortran code, but now checking before executing Fortran
-  if(opendap == 1 & (ystart < 1990 | ystart > 2015)){
-    message("currently no data on the NCI for this time window \n")
-    errors<-1
+  if(opendap == 1 & (ystart < 1990 | ystart > 2017)){
+    message("might not be data on the NCI for this time window \n")
   }
   if(DEP[2]-DEP[1]>3 | DEP[3]-DEP[2]>3){
     message("warning, nodes might be too far apart near the surface, try a different spacing if the program is crashing \n")
