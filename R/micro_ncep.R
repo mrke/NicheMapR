77ad8d2965a9afd8592542ffcb2e5eff89dc0363
mrke <mrke@unimelb.edu.au>
2020-06-07 20:16:32 +1000

    modification to micro_ncep to allow partial years to work when reading locally

--- R/micro_ncep.R
+++ R/micro_ncep.R
@@ -378,6 +378,97 @@ micro_ncep <- function(
   hourlydata = NA,
   dailyprecip = NA){ # end function parameters
 
+  loc = c(-5.3, 50.13)
+  dstart = "01/07/2016"
+  dfinish = "30/06/2017"
+  dem = NA
+  dem2 = dem
+  nyears = as.numeric(substr(dfinish, 7, 10)) - as.numeric(substr(dstart, 7, 10)) + 1
+  REFL = 0.15
+  slope = NA
+  aspect = NA
+  DEP = c(0, 2.5,  5,  10,  15,  20,  30,  50,  100,  200)
+  minshade = 0
+  maxshade = 90
+  MINSHADES = NA
+  MAXSHADES = NA
+  Refhyt = 2
+  Usrhyt = 0.01
+  Z01 = 0
+  Z02 = 0
+  ZH1 = 0
+  ZH2 = 0
+  runshade = 1
+  run.gads = 1
+  solonly = 0
+  write_input = 0
+  writecsv = 0
+  reanalysis = TRUE
+  windfac = 1
+  warm = 0
+  ERR = 1.5
+  RUF = 0.004
+  ZH = 0
+  D0 = 0
+  EC = 0.0167238
+  SLE = 0.95
+  Thcond = 2.5
+  Density = 2.56
+  SpecHeat = 870
+  BulkDensity = 1.3
+  PCTWET = 0
+  rainwet = 1.5
+  cap = 1
+  CMH2O = 1
+  hori = rep(NA, 24)
+  runmoist = 1
+  PE = rep(1.1, 19)
+  KS = rep(0.0037, 19)
+  BB = rep(4.5, 19)
+  BD = rep(BulkDensity, 19)
+  DD = rep(Density, 19)
+  maxpool = 10000
+  rainmult = 1
+  evenrain = 0
+  SoilMoist_Init = c(0.1, 0.12, 0.15, 0.2, 0.25, 0.3, 0.3, 0.3, 0.3, 0.3)
+  L = c(0, 0, 8.2, 8.0, 7.8, 7.4, 7.1, 6.4, 5.8, 4.8, 4.0, 1.8, 0.9, 0.6, 0.8, 0.4 ,0.4, 0, 0) * 10000
+  R1 = 0.001
+  RW = 2.5e+10
+  RL = 2e+06
+  PC = -1500
+  SP = 10
+  IM = 1e-06
+  MAXCOUNT = 500
+  LAI = 0.1
+  microclima.LAI = 0
+  LOR = 1
+  snowmodel = 1
+  snowtemp = 1.5
+  snowdens = 0.375
+  densfun = c(0.5979, 0.2178, 0.001, 0.0038)
+  snowmelt = 1
+  undercatch = 1
+  rainmelt = 0.0125
+  shore = 0
+  tides = 0
+  deepsoil = NA
+  rainhour = 0
+  rainhourly = 0
+  rainoff = 0
+  lamb = 0
+  IUV = 0
+  soilgrids = 0
+  IR = 0
+  message = 0
+  fail = nyears * 24 * 365
+  spatial = 'J:'
+  save = 0
+  snowcond = 0
+  intercept = 0 / 100 * 0.3
+  grasshade = 0
+  coastal = F
+  hourlydata = NA
+  dailyprecip = NA
   # error trapping - originally inside the Fortran code, but now checking before executing Fortran
   errors<-0
   if(DEP[2]-DEP[1]>3 | DEP[3]-DEP[2]>3){
@@ -791,7 +882,7 @@ micro_ncep <- function(
           dsw[dsw < 0] <- 0
           prate[prate < 0] <- 0
           prate <- prate * 3600 * 6 # mm in 6 hrs
-          ncepdata <- data.frame(obs_time = tme2[sel], Tk, Tkmin, Tkmax, sh, pr, wu, wv, dlw, ulw, dsw, tcdc) # 6-hourly ncep for chosen period plus a day added either side for interpolation
+          ncepdata <- data.frame(obs_time = tme2[sel], Tk[sel], Tkmin[sel], Tkmax[sel], sh[sel], pr[sel], wu[sel], wv[sel], dlw[sel], ulw[sel], dsw[sel], tcdc[sel]) # 6-hourly ncep for chosen period plus a day added either side for interpolation
           hourlydata <- microclima::hourlyNCEP(ncepdata = ncepdata, lat, long, tme, reanalysis) # interpolated to hourly
           cat("computing radiation and elevation effects with package microclima \n")
           microclima.out <- microclima::microclimaforNMR(lat = longlat[2], long = longlat[1], dstart = dstart, dfinish = dfinish, l = mean(microclima.LAI), x = LOR, coastal = coastal, hourlydata = hourlydata, dailyprecip = prate, dem = dem, demmeso = dem2, albr = 0, resolution = 30, zmin = 0, slope = slope, aspect = aspect, windthresh = 4.5, emthresh = 0.78, reanalysis2 = reanalysis, difani = FALSE)
