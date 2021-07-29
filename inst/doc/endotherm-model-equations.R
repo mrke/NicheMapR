e1f844bb690ea65b8c84488b12a36026334a856a
mrke <mrke@unimelb.edu.au>
2021-07-29 10:06:39 +1000

    rebuild of vignettes

--- inst/doc/endotherm-model-equations.R
+++ inst/doc/endotherm-model-equations.R
@@ -4,12 +4,12 @@ knitr::opts_chunk$set(
 )
 library(knitr)
 
-## ---- echo=FALSE, out.width = "100%", fig.align="center", fig.cap = "Figure 1. A cross section system diagram for heat flow in an animal", fig.pos='!h'----
+## ---- echo=FALSE, out.width = "100%", fig.align="center", fig.cap = "A cross section system diagram for heat flow in an animal", fig.pos='!h'----
 include_graphics("Endo_System_Diagram.jpg")
 
-## ---- echo=FALSE, out.width = "100%", fig.align="center", fig.cap = "Figure 2. The relationship between subroutines of the endoR program in NicheMapR.", fig.pos='!h'----
+## ---- echo=FALSE, out.width = "100%", fig.align="center", fig.cap = "The relationship between subroutines of the endoR program in NicheMapR.", fig.pos='!h'----
 include_graphics("EndoR_scheme.jpg")
 
-## ---- echo=FALSE, out.width = "100%", fig.align="center", fig.cap = "Figure 3. The exchange of respiratory gases entering the respiratory tract at imagainary surface 1, with carbon dioxide and water vapour added from the body and oxygen consumed according to the metabolic rate and the remainder exiting the body at imaginary surface 2. Figure modified from Porter et al. (2000)", fig.pos='!h'----
+## ---- echo=FALSE, out.width = "100%", fig.align="center", fig.cap = "The exchange of respiratory gases entering the respiratory tract at imagainary surface 1, with carbon dioxide and water vapour added from the body and oxygen consumed according to the metabolic rate and the remainder exiting the body at imaginary surface 2. Figure modified from Porter et al. (2000)", fig.pos='!h'----
 include_graphics("Gas_System_Diagram.jpg")
 
