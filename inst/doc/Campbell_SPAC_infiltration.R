b5b4e87b97a845149311c00af348993719e661d0
mrke <mrke@unimelb.edu.au>
2024-05-27 16:46:47 +1000

    adding SPAC vignette

---
+++ inst/doc/Campbell_SPAC_infiltration.R
@@ -0,0 +1,15 @@
+## ----echo = FALSE, warning=FALSE----------------------------------------------
+knitr::opts_chunk$set(
+ eval = TRUE, tidy.opts=list(width.cutoff=60), tidy=TRUE  
+)
+library(knitr)
+
+## ----echo=FALSE, out.width = "60%", fig.align="center", fig.cap = "Figure 1. A resistance (R) / water potential (psi) analog of the Soil Plant Atmosphere Continuum (SPAC) from the soil (s) through the root (r), xylem (x) and leaf (L), driving evaporation (E).", fig.pos='!h'----
+include_graphics("SPAC.jpg")
+
+## ----echo=FALSE, out.width = "40%", fig.align="center", fig.cap = "Figure 2. A resistance/potential analog of the root-soil system, including major lateral roots.", fig.pos='!h'----
+include_graphics("SPAC_figure2.jpg")
+
+## ----echo=FALSE, out.width = "40%", fig.align="center", fig.cap = "Figure 3. Potentials (psi), conductances (k), fluxes (f) and source/sinks (U) at each depth node (z).", fig.pos='!h'----
+include_graphics("SPAC_figure3.jpg")
+
