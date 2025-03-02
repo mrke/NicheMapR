9710f540d79d23aa920f2f1f4e6038f9f4fa9797
mrke <mrke@unimelb.edu.au>
2025-03-02 23:15:43 +1100

    fixing issue with reported longwave radiation in and out - had not adjusted for configuration factor (calculations were correct, just the output for QIR_IN and QIR_OUT were incorrect)

--- R/endoR_devel.R
+++ R/endoR_devel.R
@@ -779,9 +779,9 @@ endoR_devel <- function(
   SWEAT.G.S <- (QSEVAP.D + QSEVAP.V) * 0.5 / HTOVPR * 1000 # water lost from skin, g/s
   EVAP.G.S <- GEVAP + SWEAT.G.S # total evaporative water loss, g/s
   sigma <- 5.6697E-8
-  QIROUT.D <- sigma * EMISAN * AREASKIN * (TSKCALCAV.D + 273.15) ^ 4
+  QIROUT.D <- 2 * FASKY * sigma * EMISAN * AREASKIN * (TSKCALCAV.D + 273.15) ^ 4
   QIRIN.D <- QRAD.D * -1 + QIROUT.D
-  QIROUT.V <- sigma * EMISAN * AREASKIN * (TSKCALCAV.V + 273.15) ^ 4
+  QIROUT.V <- 2 * FAGRD * sigma * EMISAN * AREASKIN * (TSKCALCAV.V + 273.15) ^ 4
   QIRIN.V <- QRAD.V * -1 + QIROUT.V
 
   QSOL <- QSLR.D * DMULT + QSLR.V * VMULT # solar, W
