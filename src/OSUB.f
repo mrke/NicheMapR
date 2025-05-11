10ed9bc9397cd44989ae6d651ef8780f62b74ce9
mrke <mrke@unimelb.edu.au>
2025-05-12 01:18:02 +1000

    Fixing OSUB of microclimate model so that freezing soil temperature calcs are properly outputted. Setting PE to negative in ectotherm model and adjusting code for water balance of egg slightly.

--- src/OSUB.f
+++ src/OSUB.f
@@ -442,7 +442,8 @@ c     phase change for freezing moist soil
         endif
 1131   continue
       endif
-
+      OUT(4)=T(1)
+      OUT(14:22)=T(2:10)
 
 C     Modification by M. Kearney for effect of cloud cover on direct solar radiation, using the
 C     Angstrom formula (formula 5.33 on P. 177 of "Climate Data and Resources" by Edward Linacre 1992
@@ -543,7 +544,7 @@ c     TSKY=((QRADSK + QRADVG)/(SIGP))**(1./4.)-273
       SIOUT(1) = TIME
 C     AIR TEMPERATURE AT ANIMAL HEIGHT (1ST NODE BELOW REFERENCE HEIGHT, 200 CM)
       SIOUT(2) = OUT(34)
-C     RELATIVE HUMIDITY AT ANIMAL HEIGHT
+C     RELATIVE HUMIDITY AT REFERENCE HEIGHT
       SIOUT(3) = TAB('REL',TIME)
       IF(SIOUT(3).gt.100)then
        SIOUT(3)=100.
@@ -911,7 +912,7 @@ C       SETTING THIS MONTH'S PERCENT OF SURFACE WITH FREE WATER/SNOW ON IT
       endif
 ********************* end main code for snow model *******************************
 
-
+      tt_past=tt
       t=tt
 *************** soil moisture model ***********************
       if(runmoist.eq.1)then
@@ -1303,8 +1304,8 @@ c     end check for previous slippage
          metout(methour,19)=snowdens
          soil(methour,1)=JULDAY(DOY)
          soil(methour,2)=SIOUT(1)
-         soil(methour,3)=OUT(4)
-         soil(methour,4:12)=OUT(14:22)
+         soil(methour,3)=T(1)!OUT(4)
+         soil(methour,4:12)=T(2:10)!OUT(14:22)
          if(runsnow.eq.1)then
           sunsnow(methour,1)=JULDAY(DOY)
           sunsnow(methour,2)=SIOUT(1)
@@ -1572,8 +1573,8 @@ c     end check for previous slippage
          shadmet(methour,19)=snowdens
          shadsoil(methour,1)=JULDAY(DOY)
          shadsoil(methour,2)=SIOUT(1)
-         shadsoil(methour,3)=OUT(4)
-         shadsoil(methour,4:12)=OUT(14:22)
+         shadsoil(methour,3)=T(1)!OUT(4)
+         shadsoil(methour,4:12)=T(2:10)!OUT(14:22)
          if(runsnow.eq.1)then
           shdsnow(methour,1)=JULDAY(DOY)
           shdsnow(methour,2)=SIOUT(1)
